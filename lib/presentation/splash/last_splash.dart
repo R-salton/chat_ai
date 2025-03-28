22b57cef40c1b8f7b8246b19bb0b34b35e1c87a1
R-salton <ringusa22@gmail.com>
2025-03-28 12:48:23 +0200

    Info screen

--- lib/presentation/splash/last_splash.dart
+++ lib/presentation/splash/last_splash.dart
@@ -1,5 +1,6 @@
 import 'package:chat_ai/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LastSplash extends StatelessWidget {
   const LastSplash({super.key});
@@ -9,38 +10,115 @@ class LastSplash extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Revolutionary AI language model, transforming communication.',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: AppColors.secondBackground,
-                    fontWeight: FontWeight.w500),
+              Column(
+                children: [
+                   Text(
+              
+              textAlign: TextAlign.center,
+              'Revolutionary AI language model, transforming communication.',
+              style: TextStyle(
+                  fontSize: 30.sp,
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+              height: 20.h,
+                            ),
+                            Text(
+              textAlign: TextAlign.center,
+              'Welcome to Chat AI',
+              style: TextStyle(
+                  fontSize: 16.sp,
+                  
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+              height: 20.h,
+                            ),
+                            
+                ],
               ),
+              Container(
+                child: Column(
+                  children: [
+Image.asset(
+                'assets/images/energy.png',
+                width: 100.w,
+                height: 100.h,
+              ),
+              Text("Capabilities",style: TextStyle(
+                  fontSize: 18.sp,
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+
+                  Text(
+                    textAlign: TextAlign.center,
+                    "Remembers what user said earlier in the conversation",style: TextStyle(
+                  fontSize: 16.sp,
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text("Allows user to provide follow-up corrections",style: TextStyle(
+                  fontSize: 16.sp,
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Text("Trained to decline inappropriate requests",style: TextStyle(
+                  fontSize: 16.sp,
+                  color: AppColors.secondBackground,
+                  fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+          
               SizedBox(
-                height: 20,
+                height: 50.h,
               ),
-              Text(
-                'Chat AI is a chatbot that can help you with your daily tasks',
-                style: TextStyle(
-                    fontSize: 16,
-                    color: AppColors.secondBackground,
-                    fontWeight: FontWeight.w500),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/home');
+
+              
+             
+              
+              GestureDetector(
+                onTap: () {
+                  
                 },
-                child: Text('Get Started'),
-              )
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 200.w,
+                  height: 50.h,
+                  decoration: BoxDecoration(
+                    
+                      color: Colors.transparent,
+                      border: Border.all(color: AppColors.primary, width: 1.5.w),
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Text(
+                  
+                    textAlign: TextAlign.center,
+                    'Get started now >',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: AppColors.secondBackground,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
